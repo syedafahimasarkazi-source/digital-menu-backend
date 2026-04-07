e698937f8986839af5b14d8e26300c295ba48fd9
syedafahimasarkazi-source <syedafahima.sarkazi@gmail.com>
2026-04-07 17:30:31 +0530

    Update axios.js

--- Frontend/myapp/src/api/axios.js
+++ Frontend/myapp/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://digital-menu-backend-qm9f.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
